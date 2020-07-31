6634916136651ed50417977c334d869a292753f7
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-07-31 17:22:32 +0200

    Add logger to configuration package

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -5,7 +5,7 @@
 package configuration
 
 import (
-	"fmt"
+	"errors"
 	"github.com/google/uuid"
 	"github.com/mkyc/epiphany-wrapper-poc/pkg/environment"
 	"github.com/mkyc/epiphany-wrapper-poc/pkg/util"
@@ -27,31 +27,37 @@ type Config struct {
 }
 
 func (c *Config) CreateNewEnvironment(name string) error {
+	debug("will try to create environment %s", name)
 	env, err := environment.Create(name)
 	if err != nil {
-		panic("cannot create new environment")
+		errCreateEnvironment(err)
 	}
 	c.CurrentEnvironment = env.Uuid
+	debug("will try to save updated config %+v", c)
 	return c.Save()
 }
 
 func (c *Config) SetUsedEnvironment(u uuid.UUID) error {
+	debug("changing used environment to %s", u.String())
 	c.CurrentEnvironment = u
+	debug("will try to save updated config %+v", c)
 	return c.Save()
 }
 
 func (c *Config) GetConfigFilePath() string {
 	if usedConfigFile == "" {
-		panic("usedConfigFile not initialized") //TODO err
+		errIncorrectInitialization(errors.New("variable usedConfigFile not initialized"))
 	}
 	return usedConfigFile
 }
 
 func (c *Config) Save() error {
+	debug("will try to marshal config %+v", c)
 	data, err := yaml.Marshal(c)
 	if err != nil {
 		return err
 	}
+	debug("will try to write marshaled data to file %s", usedConfigFile)
 	err = ioutil.WriteFile(usedConfigFile, data, 0644)
 	if err != nil {
 		return err
@@ -63,10 +69,12 @@ func GetConfig() (*Config, error) {
 	usedConfigurationDirectory = path.Join(util.GetHomeDirectory(), util.DefaultConfigurationDirectory)
 	util.EnsureDirectory(usedConfigurationDirectory)
 	usedConfigFile = path.Join(usedConfigurationDirectory, util.DefaultConfigFileName)
+	debug("will try to make or get configuration")
 	return makeOrGetConfig()
 }
 
 func SetConfig(configFile string) (*Config, error) {
+	debug("will try to set not default config file at %s", configFile)
 	usedConfigurationDirectory = path.Join(util.GetHomeDirectory(), util.DefaultConfigurationDirectory)
 	util.EnsureDirectory(usedConfigurationDirectory)
 	usedConfigFile = configFile
@@ -75,27 +83,31 @@ func SetConfig(configFile string) (*Config, error) {
 
 func makeOrGetConfig() (*Config, error) {
 	if _, err := os.Stat(usedConfigFile); os.IsNotExist(err) {
+		debug("there is no config file, will try to initialize one")
 		config := &Config{
 			Version: "v1",
 			Kind:    "Config",
 		}
 		err = config.Save()
 		if err != nil {
-			panic(fmt.Sprintf("save file failed: %v\n", err)) //TODO err
+			errSave(err)
 		}
 		return config, nil
 	}
+	debug("will try to load existing config file from %s", usedConfigFile)
 	return loadConfigFromUsedConfigFile()
 }
 
 func loadConfigFromUsedConfigFile() (*Config, error) {
 	config := &Config{}
+	debug("trying to open %s file", usedConfigFile)
 	file, err := os.Open(usedConfigFile)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 	d := yaml.NewDecoder(file)
+	debug("will try to decode file %s to yaml", usedConfigFile)
 	if err := d.Decode(&config); err != nil {
 		return nil, err
 	}
