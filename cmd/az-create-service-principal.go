e13f06fcf20922e2900da591f16ac0923c9265ff
Pawel Grudzien <erzetpe@gmail.com>
2020-12-02 11:16:57 +0100

    Move create Service Principal to external command

---
+++ cmd/az-create-service-principal.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/epiphany-platform/cli/pkg/az"
+	"github.com/spf13/cobra"
+)
+
+var (
+	tenantID      string
+	subsciptionID string
+	spName        string
+)
+
+// createServicePrincipalCmd represents the createServicePrincipal command
+var createServicePrincipalCmd = &cobra.Command{
+	Use:   "create-service-principal",
+	Short: "Create Service Principal",
+	Long:  `Create Service Principal that can be used for authentication with Azure.`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("createServicePrincipal called")
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		az.CreateSP(subsciptionID, tenantID, spName)
+	},
+}
+
+func init() {
+	azCmd.AddCommand(createServicePrincipalCmd)
+
+	createServicePrincipalCmd.PersistentFlags().StringVar(&tenantID, "tenantID", "", fmt.Sprintf("TenantID of AAD where Service Principal should be created"))
+	createServicePrincipalCmd.PersistentFlags().StringVar(&subsciptionID, "subsciptionID", "", fmt.Sprintf("SubsciptionID of Subscription where Service Principal should have access"))
+	createServicePrincipalCmd.PersistentFlags().StringVar(&spName, "spName", "", fmt.Sprintf("Display Name of Service Principal"))
+}
--- cmd/az.go
+++ cmd/az.go
@@ -15,16 +15,7 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/epiphany-platform/cli/pkg/az"
 	"github.com/spf13/cobra"
-)
-
-var (
-	tenantID      string
-	subsciptionID string
-	spName        string
 )
 
 // azCmd represents the az command
@@ -36,14 +27,8 @@ var azCmd = &cobra.Command{
 	PreRun: func(cmd *cobra.Command, args []string) {
 		debug("az called")
 	},
-	Run: func(cmd *cobra.Command, args []string) {
-		az.CreateSP(subsciptionID, tenantID, spName)
-	},
 }
 
 func init() {
 	rootCmd.AddCommand(azCmd)
-	azCmd.PersistentFlags().StringVar(&tenantID, "tenantID", "", fmt.Sprintf("TenantID of AAD where Service Principal should be created"))
-	azCmd.PersistentFlags().StringVar(&subsciptionID, "subsciptionID", "", fmt.Sprintf("SubsciptionID of Subscription where Service Principal should have access"))
-	azCmd.PersistentFlags().StringVar(&spName, "spName", "", fmt.Sprintf("Display Name of Service Principal"))
 }
